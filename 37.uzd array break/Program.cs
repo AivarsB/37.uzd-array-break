53ff9ca9ad5985f55b82c4175ae0ec537c780f54
AivarsB <aivars.berzins@inbox.lv>
2020-01-14 17:06:06 +0200

    Add project files.

---
+++ 37.uzd array break/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _37.uzd_array_break
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 56, 77, 89, 34, 22, 2, 3, 4, 8 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] != 22)
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
+// Definēt int masīvu ar 10 elementiem { 1, 56, 77, 89, 34, 22, 2, 3, 4, 8 }. 
+// Drukājam vērtības console logā, tik ilgi kamēr sastopam vērtību 22. 
+// Pēc vērtības 22 sasniegšanas cikls ir jāpārtrauc.
